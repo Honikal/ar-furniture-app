c64973334178c6ff5294be4be8e38c4f6b01b614
Honikal <81885736+Honikal@users.noreply.github.com>
2025-09-20 13:13:32 -0600

    Avanzamos con lo del caso del AR y la muestra de imagenes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,10 +4,14 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
+//Importamos este schema para reconocer componentes web
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes), provideClientHydration(withEventReplay())
-  ]
+  ],
+  
 };
--- src/app/landing-page/landing-page.ts
+++ src/app/landing-page/landing-page.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+//Importamos este schema para reconocer componentes web
 
 @Component({
   selector: 'app-landing-page',
   imports: [],
   templateUrl: './landing-page.html',
-  styleUrl: './landing-page.css'
+  styleUrl: './landing-page.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]  //Permite el model viewer
 })
 export class LandingPage {
-  amount_furniture : number[] = [1,2,3];
+  furniture_models = [
+    { name: 'Mueble 1', src: '../../../public/models/1/18_9_2025.glb'},
+    { name: 'Mueble 2', src: "/models/2/18_9_2025.glb"},
+    { name: 'Mueble 3', src: "/models/3/18_9_2025.glb"}
+  ];
 
   onFurnitureClicked(index: number){
     alert(`Se ha seleccionado el objeto: ${index}`)
